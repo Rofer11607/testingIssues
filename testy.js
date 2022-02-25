ababc6ae139e5e7fdf57241a63a981eca7c3a8f1
Rofer11607 <rafac@monsterrg.com>
2022-02-25 11:48:17 -0500

    fixed users all users not being returned

--- testy.js
+++ testy.js
@@ -1,7 +1,8 @@
 const users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function getAllUsers() {
-  return users.slice(0, 5);
+  //should return all users not just a slice
+  return users
 }
 
 // Should return all users however only returning the first 5
